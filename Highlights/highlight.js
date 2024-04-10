a3f35233570c5d553f8b53c023f009084a13a653
Yousif55 <43971218+Yousif55@users.noreply.github.com>
2024-04-10 12:39:50 -0400

    Removed server side handling for Highlights page

--- Highlights/highlight.js
+++ Highlights/highlight.js
@@ -1,36 +1,206 @@
-fetch("highlight.json")
-  .then(response => response.json())
-  .then(data => {
-    const dropdown = document.getElementById('fossilDropdown');
-    const nameElement = document.getElementById('fossilName');
-    const imageElement = document.getElementById('fossilImage');
-    const descriptionElement = document.getElementById('fossilDescription');
-    const numberElement = document.getElementById('fossilNumber');
-    const ageElement = document.getElementById('fossilAge');
-    const locationElement = document.getElementById('fossilLocation');
+const data = {
+  "fossils": [
+    {
+      "name": "Ambonychia radiata",
+      "description": "Bivalve: Ambonychia radiata.",
+      "specimenNumber": "UC 697",
+      "age": "Late Ordovician",
+      "location": "Hamilton County, Cincinnati, Ohio",
+      "link": "images/Bivalve-01.jpg"
+  },
+    {
+      "name": "Ambonychia radiata 2",
+      "description": "Bivalve: Ambonychia radiata.",
+      "specimenNumber": "UC 489",
+      "age": "Late Ordovician",
+      "location": "Hamilton County, Cincinnati, Ohio",
+      "link": "images/Bivalve-02.jpg"
+    },
+    {
+      "name": "Calymene callicephala 1",
+      "description": "Trilobite: Calymene callicephala.",
+      "specimenNumber": "UC 990A",
+      "age": "Late Ordovician",
+      "location": "Hamilton County, Cincinnati, Ohio",
+      "link": "images/Trilobite-01.jpg"
+    },
+    {
+      "name": "Calymene callicephala 2",
+      "description": "Trilobite: Calymene callicephala.",
+      "specimenNumber": "UC 990A",
+      "age": "Late Ordovician",
+      "location": "Hamilton County, Cincinnati, Ohio",
+      "link": "images/Trilobite-02.jpg"
+    },
+    {
+      "name": "Cyclocystoidees illinoisensis",
+      "description": "Echinoderm: Cyclocystoidees illinoisensis.",
+      "specimenNumber": "PE 25564",
+      "age": "Ordovician",
+      "location": "Alexander County, Thebes, Illinois",
+      "link": "images/Other_Echinoderm-01.jpg"
+    },
+    {
+      "name": "Cyclonema mediale",
+      "description": "Gastropod: Cyclonema mediale.",
+      "specimenNumber": "P 6393",
+      "age": "Ordovician",
+      "location": "Hamilton County, Cincinnati, Ohio",
+      "link": "images/Gastropod-02.jpg"
+    },
+    {
+      "name": "Cyclonema varicosum",
+      "description": "Gastropod: Cyclonema varicosum.",
+      "specimenNumber": "P 6393",
+      "age": "Ordovician",
+      "location": "Butler County, Middletown, Ohio",
+      "link": "images/Gastropod-01.jpg"
+    },
+    {
+      "name": "Endoceras sp. 1",
+      "description": "Cephalopod: Endoceras sp.",
+      "specimenNumber": "UC 59937",
+      "age": "Late Ordovician",
+      "location": "Grant County, Fennimore, Wisconsin",
+      "link": "images/Cephalopod-01.jpg"
+    },
+    {
+      "name": "Endoceras sp. 2",
+      "description": "Cephalopod: Endoceras sp.",
+      "specimenNumber": "UC 15108",
+      "age": "Late Ordovician",
+      "location": "Dodge County, Trenton, Wisconsin",
+      "link": "images/Cephalopod-02.jpg"
+    },
+    {
+      "name": "Glyptocrinus dyeri",
+      "description": "Crinoid: Glyptocrinus dyeri.",
+      "specimenNumber": "P 141",
+      "age": "Ordovician",
+      "location": "Winnebago County, Rockford, Indiana",
+      "link": "images/Crinoid-02.jpg"
+    },
+    {
+      "name": "Grewingkia",
+      "description": "Rugose Coral: Grewingkia sp.",
+      "specimenNumber": "Hall 148",
+      "age": "Late Ordovician",
+      "location": "Wayne County, Richmond, Indiana",
+      "link": "images/Coral_Hall_148_Grewingkia.jpg"
+    },
+    {
+      "name": "Homotrypa dawsoni",
+      "description": "Bryozoan: Homotrypa dawsoni.",
+      "specimenNumber": "UC 208",
+      "age": "Ordovician",
+      "location": "Clinton County, Blanchester, Ohio",
+      "link": "images/Bryozoan_uc207_homotrypa_dawsoni.jpg"
+    },
+    {
+      "name": "Isotellus gigas",
+      "description": "Trilobite: Isotellus gigas.",
+      "specimenNumber": "P 181",
+      "age": "Late Ordovician",
+      "location": "Wayne County, Richmond, Indiana",
+      "link": "images/Trilobite-03.jpg"
+    },
+    {
+      "name": "Leptaena rhomboidalis",
+      "description": "Brachiopod: Leptaena rhomboidalis.",
+      "specimenNumber": "P 294.5",
+      "age": "Late Ordovician",
+      "location": "Wayne County, Richmond, Indiana",
+      "link": "images/Brachiopod-01.jpg"
+    },
+    {
+      "name": "Omospira Alexandra",
+      "description": "Gastropod: Omospira Alexandra.",
+      "specimenNumber": "PE 56766",
+      "age": "Ordovician",
+      "location": "Mercer County, High Bridge, Kentucky",
+      "link": "images/Gastropod-04.jpg"
+    },
+    {
+      "name": "Pachydictya acuta",
+      "description": "Bryozoan: Pachydictya acuta.",
+      "specimenNumber": "PE 2059",
+      "age": "Ordovician",
+      "location": "Belleville, Ontario, Canada",
+      "link": "images/Bryozoa_PE2059_Pachydictya_acuta.jpg"
+    },
+    {
+      "name": "Platystrophia ponderosa",
+      "description": "Brachiopod: Platystrophia ponderosa.",
+      "specimenNumber": "UC 22656",
+      "age": "Late Ordovician",
+      "location": "Hamilton County, Cincinnati, Ohio",
+      "link": "images/Brachiopod-02.jpg"
+    },
+    {
+      "name": "Protaster flexuosa",
+      "description": "Echinoderm: Protaster flexuosa.",
+      "specimenNumber": "PE 54844",
+      "age": "Ordovician",
+      "location": "Hamilton County, Cincinnati, Ohio",
+      "link": "images/Other_Echinoderm-02.jpg"
+    },
+    {
+      "name": "Reteocrinus onealli",
+      "description": "Crinoid: Reteocrinus onealli.",
+      "specimenNumber": "P 11905",
+      "age": "Ordovician",
+      "location": "Clinton County, Blanchester, Ohio",
+      "link": "images/Crinoid-01.jpg"
+    },
+    {
+      "name": "Urasterella sp.",
+      "description": "Echinoderm: Urasterella sp.",
+      "specimenNumber": "PE 17762",
+      "age": "Ordovician",
+      "location": "Scott County, Dorton Fort, Virginia",
+      "link": "images/Other_Echinoderm-03.jpg"
+    },
+    {
+      "name": "Zaphrentis",
+      "description": "Rugose Coral: Zaphrentis sp.",
+      "specimenNumber": "N/A",
+      "age": "Late Ordovician",
+      "location": "Jefferson County, Indiana",
+      "link": "images/Coral_Zaphrentis.jpg"
+    }
+  ]
+}
 
-    data["fossils"].forEach((fossil) => {
-      const option = document.createElement('option');
-      option.value = fossil["name"];
-      option.textContent = fossil["name"];
-      dropdown.appendChild(option);
-    });
-
-    dropdown.addEventListener('change', (event) => {
-      const selectedFossilName = event.target.value;
-      const selectedFossil = data["fossils"].find(fossil => fossil["name"] === selectedFossilName);
-
-      nameElement.textContent = selectedFossil["name"];
-      imageElement.src = selectedFossil["link"];
-      imageElement.alt = selectedFossil["name"] + " image";
-      descriptionElement.textContent = selectedFossil["description"];
-      numberElement.textContent = selectedFossil["specimenNumber"];
-      ageElement.textContent = selectedFossil["age"];
-      locationElement.textContent = selectedFossil["location"];
-    });
-  });
 
 document.addEventListener('DOMContentLoaded', function() {
+  const dropdown = document.getElementById('fossilDropdown');
+  const nameElement = document.getElementById('fossilName');
+  const imageElement = document.getElementById('fossilImage');
+  const descriptionElement = document.getElementById('fossilDescription');
+  const numberElement = document.getElementById('fossilNumber');
+  const ageElement = document.getElementById('fossilAge');
+  const locationElement = document.getElementById('fossilLocation');
+
+  data["fossils"].forEach((fossil) => {
+    const option = document.createElement('option');
+    option.value = fossil["name"];
+    option.textContent = fossil["name"];
+    dropdown.appendChild(option);
+  });
+
+  dropdown.addEventListener('change', (event) => {
+    const selectedFossilName = event.target.value;
+    const selectedFossil = data["fossils"].find(fossil => fossil["name"] === selectedFossilName);
+
+    nameElement.textContent = selectedFossil["name"];
+    imageElement.src = selectedFossil["link"];
+    imageElement.alt = selectedFossil["name"] + " image";
+    descriptionElement.textContent = selectedFossil["description"];
+    numberElement.textContent = selectedFossil["specimenNumber"];
+    ageElement.textContent = selectedFossil["age"];
+    locationElement.textContent = selectedFossil["location"];
+  });
+
   var acc = document.getElementsByClassName("accordion");
   for (var i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function() {
